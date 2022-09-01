86eaffa021004ee47565cf85c992bc1e1c9fbd78
BadMickey <joaovitorcampos161@gmail.com>
2022-08-31 22:02:34 -0300

    Pastas de Herança adicionado

---
+++ Heranca/src/Principal/FormaDuasDimensoes.java
@@ -0,0 +1,29 @@
+package Principal;
+
+public class FormaDuasDimensoes extends Forma{
+    
+    private double eixoX;
+    private double eixoY;
+
+    public FormaDuasDimensoes(String NomeDaFigura, double area, double perimetro)
+    {
+        
+    }
+    
+    public double getEixoX() {
+        return eixoX;
+    }
+
+    public void setEixoX(double eixoX) {
+        this.eixoX = eixoX;
+    }
+
+    public double getEixoY() {
+        return eixoY;
+    }
+
+    public void setEixoY(double eixoY) {
+        this.eixoY = eixoY;
+    }
+
+}
